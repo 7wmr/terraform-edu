ec329d416c9a3981c6ff8393323cf3f41a3a1d8f
Richard Neave <Richard.Neave@ipsoft.com>
2019-07-09 15:45:30 +0100

    terraform create digitalocean vm

--- digitalocean.tf
+++ digitalocean.tf
@@ -1,13 +1,16 @@
 # Set the variable value in *.tfvars file
 # or using -var="do_token=..." CLI option
-variable "do_token" {}
+# 1variable "do_token" {} # using env variable DIGITALOCEAN_TOKEN
 
 # Configure the DigitalOcean Provider
 provider "digitalocean" {
-  token = "${var.do_token}"
+  # token = "${var.do_token}"
 }
 
 # Create a web server
 resource "digitalocean_droplet" "web" {
-  # ...
+  name   = "ubuntu-dev01"
+  size   = "s-1vcpu-1gb"
+  image  = "ubuntu-18-04-x64"
+  region = "lon1"
 }
