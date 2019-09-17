9d84d102fe6f82376cf3b677287422b8be710c14
Richard Neave <neaverds@icloud.com>
2019-09-17 15:21:36 +0100

    RABBITMQ
    - adding rabbitmq1

---
+++ stage/messaging/rabbitmq/main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  profile    = "default"
+  region     = var.aws_region
+}
+
+terraform {
+  backend "s3" {
+    bucket  = "terraform-edu"
+    region  = "eu-west-2"
+    key     = "stage/messaging/rabbitmq/terraform.tfstate"
+    encrypt = true    
+  }
+}
+
+module "messaging_rabbitmq" {
+  source = "git@github.com:7wmr/terraform-module-edu.git//messaging/rabbitmq?ref=master"
+    
+  aws_region   = var.aws_region
+
+  rabbitmq     = var.rabbitmq
+ 
+  key_name     = var.key_name
+  cluster_name = var.cluster_name
+}
+
---
+++ stage/messaging/rabbitmq/variables.tf
@@ -0,0 +1,28 @@
+variable "key_name" {
+  type        = string
+  description = "SSH key name"
+  default     = "ipsoft-macbook"
+}
+
+variable "cluster_name" {
+  type        = string
+  description = "Name of cluster to be created."
+  default     = "edu-cluster"  
+}
+
+variable "aws_region" {
+  type        = string
+  description = "AWS region to be used"
+  default     = "eu-west-2"
+}
+
+variable "rabbitmq" {
+  type         = any
+  description  = "RabbitMQ arguments"
+  default      = {
+    username   = "admin"
+    password   = "admin"
+  }
+}
+
+
