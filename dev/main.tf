f5fe1c3a1e2a9161fe23171527362a3bbb6b4afa
Richard Neave <neaverds@icloud.com>
2019-09-23 16:37:04 +0100

    CERTIFICATES
    - comment test

--- dev/main.tf
+++ dev/main.tf
@@ -12,11 +12,11 @@ terraform {
   }
 }
 
-resource "aws_iam_server_certificate" "test_cert" {
-  name             = "some_test_cert"
-  certificate_body = "${file("self-ca-cert.pem")}"
-  private_key      = "${file("test-key.pem")}"
-}
+#resource "aws_iam_server_certificate" "test_cert" {
+#  name             = "some_test_cert"
+#  certificate_body = "${file("self-ca-cert.pem")}"
+#  private_key      = "${file("test-key.pem")}"
+#}
 
 
 
