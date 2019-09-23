9e218e794aeb6729788d3232f272448941c527ef
Richard Neave <neaverds@icloud.com>
2019-09-23 16:30:25 +0100

    CERTIFICATE
    - testing

--- dev/main.tf
+++ dev/main.tf
@@ -12,6 +12,14 @@ terraform {
   }
 }
 
+resource "aws_iam_server_certificate" "test_cert" {
+  name             = "some_test_cert"
+  certificate_body = "${file("self-ca-cert.pem")}"
+  private_key      = "${file("test-key.pem")}"
+}
+
+
+
 module "msg_rabbitmq" {
   source   = "git@github.com:7wmr/terraform-module-edu.git//msg/rabbitmq?ref=master"
     
