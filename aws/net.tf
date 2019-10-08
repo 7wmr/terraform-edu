4dbe00664da921eb6874e3c2ee05b9d1bfd6c28e
Richard Neave <neaverds@icloud.com>
2019-10-08 11:13:08 +0100

    NETWORK
    - investigate network issues

--- aws/net.tf
+++ aws/net.tf
@@ -67,7 +67,7 @@ resource "aws_route_table" "main" {
   vpc_id = "${aws_vpc.main.id}"
   
   route {
-    cidr_block = "10.0.0.0/16"
+    cidr_block = "0.0.0.0/0"
     gateway_id = "${aws_internet_gateway.main.id}"
   }
 
